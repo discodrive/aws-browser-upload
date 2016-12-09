037d06a259e6fb52f93ff16f4e48d819fa09f12f
Lee <lee.a@substrakt.co.uk>
2016-12-09 21:52:07 +0000

    UPDATE: Enqueue scripts method removing register scripts

--- app/setup.php
+++ app/setup.php
@@ -5,13 +5,10 @@ add_action('admin_menu', 'abu_admin_menu');
 // Enqueue Javascript to backend
 if (! function_exists('abu_enqueue_scripts')) {
     function abu_enqueue_scripts() {
-        wp_register_script('sdk', '/wp-content/plugins/aws-browser-upload/assets/js/sdk.js', array('jquery'));
-        wp_register_script('aws-sdk', 'https://sdk.amazonaws.com/js/aws-sdk-2.6.10.min.js');
-        wp_register_style('abu-styles', '/wp-content/plugins/aws-browser-upload/assets/css/abu.css');
-
-        wp_enqueue_script('sdk');
-        wp_enqueue_script('aws-sdk');
-        wp_enqueue_style('abu-styles');
+        wp_enqueue_script('sdk', '/wp-content/plugins/aws-browser-upload/assets/js/sdk.js', array('jquery'));
+        wp_enqueue_script('aws-sdk', 'https://sdk.amazonaws.com/js/aws-sdk-2.6.10.min.js');
+        wp_enqueue_style('abu-styles', '/wp-content/plugins/aws-browser-upload/assets/css/abu.css');
+        wp_enqueue_style('clipboard', '/wp-content/plugins/aws-browser-upload/assets/js/clipboard.min.js');
     }
     add_action('admin_enqueue_scripts', 'abu_enqueue_scripts');
 }
